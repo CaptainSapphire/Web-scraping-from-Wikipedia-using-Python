ab90cd8b81de19e52b4b5cd408ec9e46dfc6d4dd
CaptainSapphire <mpharry9999@gmail.com>
2026-08-18 16:14:58 -0700

    finished formatting code from geeks4geeks!

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ print(sys.executable)
 
 import requests # It is the foundation of web scraping in Python. It allows to send HTTP requests like GET and POST to download web pages quickly and reliably.
 
-import lxml #  A high-performance library for parsing HTML and XML, lxml is extremely fast and ideal for large-scale scraping tasks
-from bs4 import BeautifulSoup # A beginner-friendly HTML parser that creates a parse tree for easy data extraction. It works seamlessly with content fetched using Requests.
-from selenium import webdriver # Automates a real browser, enabling scraping of dynamic, JavaScript-loaded websites. It is slower than other tools and less suitable for large-scale scraping.
+import lxml #  for parsing HTML and XML, lxml is extremely fast and ideal for large-scale scraping tasks
+from bs4 import BeautifulSoup # HTML parser that creates a parse tree for easy data extraction. It works seamlessly with content fetched using Requests.
+from selenium import webdriver # Automates a real browser, enabling scraping of dynamic, JavaScript-loaded websites
 import scrapy # A full-featured, asynchronous web scraping framework built for speed and scalability. It is ideal for large crawling projects, providing pipelines and advanced data handling capabilities.
 
-# Note: I had to change the imports a lot because the original site is outdated. agh. 
+# Note: I had to change the imports a lot because the original site is outdated and I was struggling. 
 
 ## Workflow description
 ### document load --> parsing --> extraction --> transformation
@@ -25,11 +25,10 @@ print(page.content) # page.content: returns the full HTML of the page.
 
 
 # parsing html
-
-url = "https://en.wikipedia.org/wiki/Main_Page"
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
-page = requests.get(url, headers=headers)
 soup = BeautifulSoup(page.content, 'html.parser')
 print(soup.prettify())
+
+# extra the actual contents
+print(soup.find_all('p'))
+print("\n\n")
+print(soup.find_all('p')[0].get_text())
